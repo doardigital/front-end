e737805ba3b9ef06e123c4bc4050f4e7162c9d42
Stephany carolina <ster.carolinas@hotmail.com>
2023-06-06 17:13:22 -0300

    alteração relatorio

--- script.js
+++ script.js
@@ -19,6 +19,23 @@ button.addEventListener("click", () => {
 
 
 
+  /*relatório*/ 
+
+  // Após o usuário fazer login
+// Supondo que você tenha o email do usuário armazenado na variável "emailUsuarioLogado"
+if (emailUsuarioLogado === 'admin@doardigital.com') {
+  $('#relatorio-action').show(); // Mostra o item de menu "Relatório"
+} else {
+  $('#relatorio-action').hide(); // Oculta o item de menu "Relatório"
+}
+
+
+
+if (emailUsuarioLogado === 'admin@doardigital.com') {
+  $('.menu-quero-doar').remove(); // Remove o item de menu "Quero doar"
+  $('.menu-minhas-doacoes').remove(); // Remove o item de menu "Minhas doações"
+}
+
 
 
 
